a2ffb237fc2f56247b0c2d11248695bada15c166
FERHAT <ferhatkuten@gmail.com>
2023-03-07 19:26:08 +0300

    add service

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,12 @@ import { wrapper } from '@/store/store'
 export default function App({ Component, ...rest }) {
   const {store, props} = wrapper.useWrappedStore(rest)
   const {pageProps} = props
+
+  
   return (
     <Provider store = {store}>
       <Component{...pageProps} />
+      
     </Provider>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,19 @@
 import { count, increment } from '@/store/Slices/CounterSlices';
 import styles from '@/styles/Home.module.css'
 import { useSelector, useDispatch } from 'react-redux'
-
+import React, { useEffect, useState} from 'react'; 
+import axios from 'axios';
 
 export default function Home() {
   const dispatch = useDispatch();
   const counter = useSelector(count);
+  
+  const [service, setService] = useState ([])
+  useEffect(() => {
+    axios.get('https://us-central1-analytics-d2e71.cloudfunctions.net/login').then(response => setService(response.data))
+  }, []);
+
+    console.log(service);
 
   return (
     <>
@@ -15,6 +23,9 @@ export default function Home() {
         <div className={styles.buttonContainer}>
           <button onClick={() => dispatch(increment(1))}> Counter </button>
         </div>
+      </div>
+      <div className= {styles.serviceContainer}>
+        <h2> message : {service.message}</h2>
       </div>
     </>
   )
